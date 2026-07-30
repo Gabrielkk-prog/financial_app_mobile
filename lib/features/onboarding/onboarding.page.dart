cf4a1fa8c730d295e9ee9f14fa5894d2f8b24608
joao gabriel da silva passos nascimento <lendariojohn@gmmail.como.>
2026-07-30 20:07:12 -0300

    day 6 on develop (just refatoring)

--- lib/features/onboarding/onboarding.page.dart
+++ lib/features/onboarding/onboarding.page.dart
@@ -1,3 +1,5 @@
+//on this page I configure the onboading settings!
+import 'package:financial_app_project/commom/widgets/primary_button.dart';
 import 'package:flutter/material.dart';
 import 'package:financial_app_project/commom/constants/app_colors.dart';
 import 'package:financial_app_project/commom/constants/app_text_styles.dart';
@@ -9,17 +11,15 @@ class OnboardingPage extends StatelessWidget {
 @override
 Widget build(BuildContext context) {
   return Scaffold(
+    backgroundColor: AppColors.iceWhite,
     body: Align(
         child: Column(
           children: [      // chlidren organize the widgets in a vertical layout by order of their appearance in the list
-            const SizedBox(height: 60.0),
+            const SizedBox(height: 48.0),
             Expanded(
               flex: 2,             // This container will take 2/3 of the available space
-             child: Container(
-               color: AppColors.iceWhite,
-             child: Image.asset('assets/images/man.png.png'),
-          ),
-        ),
+              child: Image.asset('assets/images/man.png.png'),
+            ),
             Text('Spend Smarter', style: AppTextStyles.mediumText.copyWith(
               color: AppColors.greenlightTwo,
             ),
@@ -28,18 +28,24 @@ Widget build(BuildContext context) {
               color: AppColors.greenlightTwo,
             ),
             ),
-            const SizedBox(height: 16.0), 
-            PrimaryButton(
-              text: 'Get Started',
-              onPressed: () {},       //turn on the grey color function 
+            
+            Padding(
+              padding: const EdgeInsets.symmetric(
+                horizontal: 32.0,
+                vertical: 16.0,
+                ),                   
+              child: PrimaryButton(
+                text: 'Get Started',
+                onPressed: () {},       //turn on the grey color function 
+              ),
             ),
-            const SizedBox(height: 16.0),                   // Spacing Down
+                        
             Text (
               'Already have an account? Log In' , style: AppTextStyles.smallText.copyWith(
               color: AppColors.grey,
             ),
             ),
-            const SizedBox(height: 40.0),
+            const SizedBox(height: 24.0),
           ]
          )
       ),
